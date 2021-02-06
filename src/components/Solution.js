a6fb1bc4def14202fbcd0f2a484a6946289a1e99
etsolmon <e.tsolmon@gmail.com>
2021-02-06 11:33:57 +0800

    description of commit new

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -10,39 +10,21 @@ import { Link } from 'react-router-dom';
 function Solution() {
   return (
     <>
-      <div className='testrow'>
-        <div className='testcolumn testleft'>
-          <h2>Column 1</h2>
-          <p>Some text..</p>
-        </div>
-        <div className='testcolumn testright'>
-          <h2>Column 2</h2>
-          <p>Some text..</p>
-        </div>
-      </div>
-      <div className='testrow'>
-        <div className='testcolumn testleft'>
-          <h2>Column 1</h2>
-          <p>Some text..</p>
-        </div>
-        <div className='testcolumn testright'>
-          <h2>Column 2</h2>
-          <p>Some text..</p>
-        </div>
-      </div>
-      <div className='testrow'>
-        <div className='testcolumn testleft'>
-          <h2>Column 1</h2>
-          <p>Some text..</p>
-        </div>
-        <div className='testcolumn testright'>
-          <h2>Column 2</h2>
-          <p>Some text..</p>
-        </div>
+      <div class="cards">
+        <div class="card">ХАМГИЙН ШУУРХАЙ ХҮРГЭЛТ</div>
+        <div class="card">TWO</div>
+        <div class="card">THREE</div>
+        <div class="card">FOUR</div>
+        <div class="card">FIVE</div>
+        <div class="card">SIX</div>
+        <div class="card">SEVEN</div>
+        <div class="card">EIGHT</div>
+        <div class="card">NINE</div>
+        <div class="card">TEN</div>
+        <div class="card">ELEVEN</div>
+        <div class="card">TWELVE</div>
       </div>
     </>  
-      
-
   );
 }
 export default Solution;
